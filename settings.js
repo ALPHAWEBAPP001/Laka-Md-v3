3ac40cc9ab66224d8c8a010754ed39575ccfb2ff
ALPHAWEBAPP001 <githubwebappdeploy@gmail.com>
2025-07-13 15:19:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=0kJ3lRLA#dWFuZB92KR_hOjgqaaNyoVwc-6px1zCvZJxxSAp3p3g",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=MkY3STSR#SOrDnjovxCbZxzWPWfhXaGJo6s1W8Dj-NauFtmg_h3g",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
